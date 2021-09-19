c9530b4f33c8df2cd87455f81b6258be43a67aab
Yassine ELMERZOUKI <yassinemerzouki.ym@gmail.com>
2021-09-19 23:19:15 +0100

    update city component

--- client/src/components/City.js
+++ client/src/components/City.js
@@ -9,7 +9,8 @@ query getCity($name:String){
     name,
     id,
     image{url},
-    description
+    description,
+    map
     }
 }
 `
@@ -35,7 +36,7 @@ const City = ()=>{
                                     <p>{data.cities[0].description}</p>
                                 </div>
                                 <div className="map col-sm-6">
-                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13235101.686657706!2d-14.067040277208973!3d30.518392794903168!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd0b88619651c58d%3A0xd9d39381c42cffc3!2sMaroc!5e1!3m2!1sfr!2sma!4v1630582688390!5m2!1sfr!2sma" height={450} style={{border:0,width:100+'%',borderRadius:0.5+'rem',boxShadow:'0 0 10 rgba(0,0,0,.25)'}} allowfullscreen="" loading="lazy"></iframe>
+                                <iframe src={data.cities[0].map} height={450} style={{border:0,width:100+'%',borderRadius:0.5+'rem',boxShadow:'0 0 10 rgba(0,0,0,.25)'}} allowfullscreen="" loading="lazy"></iframe>
                                 </div>   
                             </div>
                         </div>
